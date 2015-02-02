ed5679e781c8a19befc3bfbd98c05a3b4a250bf2
lazarus  morris <lazarusmorris@ymail.com>
2015-02-02 12:37:46 -0800

    no message

--- controller/create-db.php
+++ controller/create-db.php
@@ -5,10 +5,19 @@ $connection = new mysqli($host,$username,$password);
 if($connection->connect_error){
   die("Error:" . $connection->connect_error);  
 }
-else{
-    echo "Success:" . $connection->host_info ;
+$exists=$connection->select_db();
+
+
+
+$connection->close($databse);
+if($exists){
+ $query = $connection->query("CREATE DATABASE $databse");
+ if($query){
+     echo"Successfully created database:".$databse;
+     
+ }
 }
-
-
-
-$connection->close();
+else{
+    echo "Database has already been created";
+    
+}
