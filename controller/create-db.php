de3d18af7b9607ae520f8c317279fcf4a4eec6e4
lazarus  morris <lazarusmorris@ymail.com>
2015-02-04 13:58:59 -0800

    no message

--- controller/create-db.php
+++ controller/create-db.php
@@ -11,7 +11,7 @@ $exists=$connection->select_db();
 
 $connection->close($databse);
 if($exists){
- $query = $connection->query("CREATE DATABASE $databse");
+ $query = $connection->$query("CREATE DATABASE $databse");
  if($query){
      echo"Successfully created database:".$databse;
      
@@ -21,6 +21,13 @@ else{
     echo "Database has already been created";
     
 }
+$query = $connection->query("Create Table posts("
+        . "id int(11) NOT NULL AUTO_INCREMENT "
+        . "title varchar (225) NOT NULL"
+        . "post text NOT NULL"
+        . "PRIMARY KEY (id)");
+        
+
 
 $connection->close();
         
