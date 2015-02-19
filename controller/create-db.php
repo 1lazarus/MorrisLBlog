df3a27f479ba308776a03a46c8ef20a5b53912b5
lazarus  morris <lazarusmorris@ymail.com>
2015-02-19 14:04:12 -0800

    no message

--- controller/create-db.php
+++ controller/create-db.php
@@ -3,30 +3,7 @@
 require_once (__DIR__ . "/../model/config.php");
 
 
-$connection = new mysqli($host, $username, $password);
 
-
-
-
-if ($connection->connect_error) {
-    die("<p>Error:" . $connection->connect_error . "</p>");
-}
-
-
-$exists = $connection->select_db($database);
-
-
-if (!$exists) {
-    $query = $connection->query("CREATE DATABASE $database");
-   
-    
-    if ($query) {
-        echo"<p>Successfully created database:" . $database . "<p/>";
-    }
-} 
-else {
-    echo "<p>Database has already been created</p>";
-}
 
 $query = $connection->query("CREATE TABLE posts("
         . "id int(11) NOT NULL AUTO_INCREMENT,"
